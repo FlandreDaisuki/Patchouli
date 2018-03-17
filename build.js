51e8904e7d89db6862da5a246b8cd753ebf621c8
FlandreDaisuki <vbnm123c@gmail.com>
2018-03-17 19:59:32 +0800

    Make build more easier

--- build.js
+++ build.js
@@ -1,6 +1,9 @@
-const { writeFileSync, readFileSync } = require('fs');
+const { writeFileSync, readFileSync, readdirSync, unlinkSync } = require('fs');
 
 const rollup = require('rollup');
+const acorn = require('acorn');
+const walk = require('acorn/dist/walk');
+const MagicString = require('magic-string');
 const vue = require('rollup-plugin-vue');
 const userscriptCSS = require('rollup-plugin-userscript-css');
 const metablock = require('rollup-plugin-userscript-metablock');
@@ -13,6 +16,7 @@ const globals = {
   vuex: 'Vuex',
   'vue-i18n': 'VueI18n'
 };
+
 
 async function preBuild() {
   const bundle = await rollup.rollup({
@@ -40,6 +44,40 @@ async function preBuild() {
   console.log('✔️ prepare build');
 }
 
+function removeDebuggers(code) {
+  const ast = acorn.parse(code, { ecmaVersion: 9, ranges: true });
+  const ms = new MagicString(code);
+
+  walk.simple(ast, {
+    FunctionDeclaration(node) {
+      while (code[node.start - 1] === ' ') {
+        node.start -= 1;
+      }
+      while (code[node.end] !== '\n') {
+        node.end += 1;
+      }
+      node.end += 1; // remove '\n'
+      if (node.id.name === '$debug') {
+        ms.remove(node.start, node.end);
+      }
+    },
+    CallExpression(node) {
+      if (node.callee.name === '$debug') {
+        while (code[node.start - 1] === ' ') {
+          node.start -= 1;
+        }
+        while (code[node.end] !== '\n') {
+          node.end += 1;
+        }
+        node.end += 1; // remove '\n'
+        ms.remove(node.start, node.end);
+      }
+    }
+  });
+
+  return ms.toString();
+}
+
 async function buildRel() {
   const bundle = await rollup.rollup({
     input: 'dist/index.js',
@@ -54,11 +92,14 @@ async function buildRel() {
     external,
   });
 
-  await bundle.write({
-    file: 'dist/patchouli.user.js',
+  const { code } = await bundle.generate({
     format: 'iife',
     globals,
   });
+
+  const relCode = removeDebuggers(code);
+
+  writeFileSync('dist/patchouli.user.js', relCode);
 
   console.log('✔️ build userscript for release');
 }
@@ -84,9 +125,40 @@ async function buildDev() {
 
   console.log('✔️ build userscript for develop');
 }
+function updateReadme() {
+  ['README.md', 'README.en.md'].forEach(file => {
+    const readme = readFileSync(file, 'utf8');
+    const replaced = readme.replace(/latest-v[\d.]+/g, `latest-v${pkg.version}`);
+    writeFileSync(file, replaced);
+
+    const lines = replaced.split(/\r?\n/);
+    const changelogIdx = lines.findIndex(line => line.includes('## Changelog'));
+    let hasChangelog = false;
+    for (const line of lines.slice(changelogIdx, -1)) {
+      if (line.includes(pkg.version)) {
+        hasChangelog = true;
+        break;
+      }
+    }
+    if (!hasChangelog) {
+      console.warn('🛠 No changelog in current version.');
+    }
+  });
+}
+
+function cleanBuildDir() {
+  const files = readdirSync('dist');
+  for (const f of files) {
+    if (f.endsWith('.js') || f.endsWith('.css')) {
+      unlinkSync(`dist/${f}`);
+    }
+  }
+}
 
 // entry
+cleanBuildDir();
 preBuild().then(() => {
   buildRel();
   buildDev();
+  updateReadme();
 });
